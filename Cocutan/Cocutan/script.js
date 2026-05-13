90bcd2e0dd9ca52acfafad494aed5b643541006e
Frog_slime <karmarin.wui1@gmail.com>
2026-05-13 16:44:28 +0300

    Add files via upload

---
+++ Cocutan/Cocutan/script.js
@@ -0,0 +1,7 @@
+/* ====== SMOOTH SCROLL TO SECTION ====== */
+function scrollToSection(id) {
+  const el = document.getElementById(id);
+  if (el) {
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
